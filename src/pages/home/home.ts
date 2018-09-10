4d5bf8e0ac32ed395881b26b92a667f6d57e0fb3
Francisco Palomares <frajepala96@gmail.com>
2018-09-10 12:13:24 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var $: any;
+declare var window: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+
+  posts = [];
+  imageUrl: string = 'assets/imgs/profile/perfil.jpg';
+  mostrar_titulo: boolean;
   constructor(public navCtrl: NavController) {
+    for (let i = 0; i < 10; i++) {
+      this.posts[i] = {
+        text: 'Canción ' + i,
+        created_at: (i + 1),
+      };
+    }
+
+    this.mostrar_titulo = false;
+
+
 
   }
 
+  ngAfterViewInit() {
+    $('#titulo').hide();
+
+  }
+
+
+  onScroll($event) {
+    console.log($event.scrollTop)
+
+    if ($event.scrollTop > 290) {
+      // this.mostrar_titulo = true;
+      //$('#titulo').show(600);
+      $('#titulo').fadeIn("slow");
+    }
+    else {
+      // this.mostrar_titulo = false;
+      //$('#titulo').hide(600);
+      $('#titulo').fadeOut("slow");
+    }
+  }
+
+
 }
+
+
